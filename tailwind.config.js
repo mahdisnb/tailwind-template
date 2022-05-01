a1397f8eb799260fb69a2042e64ed31f94ab50db
mahdi msb <mahdiimsb@gmail.com>
2022-05-01 11:49:03 +0430

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height:{
+        '45':'45rem',
+        '27':'27rem',
+      },
+    },
+  },
+  plugins: [],
+}
